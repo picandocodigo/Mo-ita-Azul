2cb1f96e22c8c7668ab572da089577fa24a50f22
Fernando Briano <fernando@picandocodigo.net>
2011-12-03 20:47:58 -0200

    ¿Por qué María? ¿Por quéééé?

--- source/controllers/home.php
+++ source/controllers/home.php
@@ -9,11 +9,9 @@ class Home extends Controller {
 	public function index() {
 		Loader::model("balance");
 		$balance = new BalanceModel();
-		var_dump($balance->getBalance());
-
 		$inView = new View("home/index");
 		$inView->setData(array("hola" => array("PEPE")));
-		$js = array();
+		$js = array('jquery-1.7.1.min', 'highcharts');
 		$css = array('screen', 'app');
 		View::defaultLayoutRender($inView, "Home", true, $js, $css);
 	}
