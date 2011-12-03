f2caed271735e5fd1177b445591196c740fc9ee3
JuanMa Pereyra <jmpereyra@gmail.com>
2011-12-03 18:23:29 -0200

    Van modificaciones

---
+++ source/config/database.php
@@ -0,0 +1,7 @@
+<?php
+define("_DB_HOST", "10.1.0.9");
+define("_DB_PORT", 3306);
+define("_DB_NAME", "monita");
+define("_DB_USER", "monita");
+define("_DB_PASS", "azul");
+define("_DB_SYST", "MySQL");
