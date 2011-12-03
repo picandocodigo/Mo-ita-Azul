269a20bf3f4ecb9bbf5d4f8c6b21eb44b4a1a772
JuanMa Pereyra <jmpereyra@gmail.com>
2011-12-03 18:32:40 -0200

    Arreglos varios

--- source/controllers/home.php
+++ source/controllers/home.php
@@ -7,6 +7,11 @@ class Home extends Controller {
 	}
 	
 	public function index() {
-		echo "dale que va";
+		
+		$inView = new View("home/index");
+		$inView->setData(array("hola" => array("PEPE")));
+		$js = array();
+		$css = array();
+		View::defaultLayoutRender($inView, "Home", true, $js, $css);
 	}
 }
