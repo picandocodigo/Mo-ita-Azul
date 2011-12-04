f93bc96e40450a6e948a6c6051b27fe0d1030ae0
Fernando Briano <fernando@picandocodigo.net>
2011-12-03 22:07:43 -0200

    Mirá mamá, estoy programando con JavaScript

--- source/views/home/index.tpl.php
+++ source/views/home/index.tpl.php
@@ -1,2 +1,15 @@
-<?php
-?>
+<?php ?>
+<div id="container"> </div>
+<script>
+	
+	loadChart('Test',[
+		<?php 
+			foreach ($balances as $balance) : ?>
+		{
+            name: '<?php echo "{$balance->name} {$balance->year}" ; ?>',
+            data: [<?php echo "{$balance->credit} , {$balance->expenses}"; ?>]
+        },
+         <?php endforeach; ?>
+         ]);
+         	
+</script>
